ecc5cdbf4ccd0e2bd8b5eec2040349e258364be7
BitForgeCraftedCode <11381086+BitForgeCraftedCode@users.noreply.github.com>
2023-04-19 13:46:32 -0400

    fix a few more typos

--- src/pages/about.js
+++ src/pages/about.js
@@ -13,11 +13,11 @@ const AboutPage = () => {
           <Col xs={12} md={6} lg={6}>add an image here</Col>
           <Col xs={12} md={6} lg={6}>
             <p>
-              Hi, I'm T.J. Reid founder of Mr. FixIt LLC. I have been working in construction since 2000 
+              Hi, I'm T.J. Reid founder of Mr. Fix It LLC. I have been working in construction since 2000 
               and have over 20 years of experience! I strive to make my clients happy and would love to 
               help make your renovation or project a reality.
             </p>
-            <p>Mr. FixIt is located in Salida Colorado</p>
+            <p>Mr. Fix It is located in Salida Colorado</p>
             <p><Link to="/contact">Contact me</Link> to for a free estimate and let's talk about how I can help you.</p>
           </Col>
         </Row>
