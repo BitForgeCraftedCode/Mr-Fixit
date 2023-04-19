a957e68171dec27a0c8b5e4e80b43d844274e9f0
BitForgeCraftedCode <11381086+BitForgeCraftedCode@users.noreply.github.com>
2023-04-19 11:13:39 -0400

    add site url to metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
   siteMetadata: {
     title: `Mr. Fixit Handyman Service`,
     description: `Your go to handyman for all kinds of projects. Services include general handyman, kitchen and bath, tile, panting, and carpentry.`,
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: `https://mrfixitsalida.com/`,
   },
   plugins: [],
 }
