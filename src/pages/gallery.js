dab479fa054d98ef82b0b7f06977874606f9109f
BitForgeCraftedCode <11381086+BitForgeCraftedCode@users.noreply.github.com>
2023-05-11 13:44:33 -0400

    add starter structure for gallery

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,6 +1,9 @@
 import * as React from "react"
 import { StaticImage } from "gatsby-plugin-image"
 import { graphql } from 'gatsby'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 import Layout from '../components/layout'
 import { Seo } from "../components/seo"
 
@@ -8,7 +11,64 @@ const GalleryPage = ({data}) => {
 	console.log(data)
 	return (
 		<Layout>
-			add gallery images here
+			<Container fluid className="pt-5">
+				<Row>
+					<Col>
+						<StaticImage src="../images/services/handyman.jpg" alt="handy man" />
+					</Col>
+					<Col>
+						<StaticImage src="../images/services/handyman.jpg" alt="handy man" />
+					</Col>
+					<Col>
+						<StaticImage src="../images/services/handyman.jpg" alt="handy man" />
+					</Col>
+				</Row>
+				<Row>
+					<Col>
+						<StaticImage src="../images/services/handyman.jpg" alt="handy man" />
+					</Col>
+					<Col>
+						<StaticImage src="../images/services/handyman.jpg" alt="handy man" />
+					</Col>
+					<Col>
+						<StaticImage src="../images/services/handyman.jpg" alt="handy man" />
+					</Col>
+				</Row>
+				<Row>
+					<Col>
+						<StaticImage src="../images/services/handyman.jpg" alt="handy man" />
+					</Col>
+					<Col>
+						<StaticImage src="../images/services/handyman.jpg" alt="handy man" />
+					</Col>
+					<Col>
+						<StaticImage src="../images/services/handyman.jpg" alt="handy man" />
+					</Col>
+				</Row>
+				<Row>
+					<Col>
+						<StaticImage src="../images/services/handyman.jpg" alt="handy man" />
+					</Col>
+					<Col>
+						<StaticImage src="../images/services/handyman.jpg" alt="handy man" />
+					</Col>
+					<Col>
+						<StaticImage src="../images/services/handyman.jpg" alt="handy man" />
+					</Col>
+				</Row>
+				<Row>
+					<Col>
+						<StaticImage src="../images/services/handyman.jpg" alt="handy man" />
+					</Col>
+					<Col>
+						<StaticImage src="../images/services/handyman.jpg" alt="handy man" />
+					</Col>
+					<Col>
+						<StaticImage src="../images/services/handyman.jpg" alt="handy man" />
+					</Col>
+				</Row>
+
+			</Container>
 		</Layout>
 	)
 }
