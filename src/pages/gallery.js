4f06a6b8df0e5a4c029e7f85c2ef5b5ac0d5cb9b
BitForgeCraftedCode <11381086+BitForgeCraftedCode@users.noreply.github.com>
2023-05-11 12:17:16 -0400

    add gallery page

---
+++ src/pages/gallery.js
@@ -0,0 +1,18 @@
+import * as React from "react"
+import { StaticImage } from "gatsby-plugin-image"
+import Layout from '../components/layout'
+import { Seo } from "../components/seo"
+
+const GalleryPage = () => {
+	return (
+		<Layout>
+			add gallery images here
+		</Layout>
+	)
+}
+
+export default GalleryPage
+
+export const Head = () => (
+	<Seo />
+)
