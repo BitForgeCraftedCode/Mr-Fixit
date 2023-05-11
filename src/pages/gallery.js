d9b180de77b8ce956dd84c0422b867f8f2ec7911
BitForgeCraftedCode <11381086+BitForgeCraftedCode@users.noreply.github.com>
2023-05-11 13:28:27 -0400

    add graphql query for img to gallery

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,15 +1,30 @@
 import * as React from "react"
 import { StaticImage } from "gatsby-plugin-image"
+import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 import { Seo } from "../components/seo"
 
-const GalleryPage = () => {
+const GalleryPage = ({data}) => {
+	console.log(data)
 	return (
 		<Layout>
 			add gallery images here
 		</Layout>
 	)
 }
+
+export const query = graphql`
+    query {
+      allFile(filter: {relativeDirectory: {eq: "gallery"}}) {
+        nodes {
+          childImageSharp {
+            gatsbyImageData( layout: FULL_WIDTH, formats: AUTO)
+          }
+          relativeDirectory
+        }
+      }
+    }
+  `
 
 export default GalleryPage
 
